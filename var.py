f3fc9cfa91e9167e25f92314006c3ac11164886f
cactusjackk7 <cactusjackk7@gmail.com>
2021-01-12 22:41:13 +0100

    swap varaibles

---
+++ var.py
@@ -0,0 +1,16 @@
+# python program to swap 2 vars
+
+x = 5
+y = 10
+
+# to take inputs from the user
+#x = input('Enter value of x: ')
+#y = input('Enter value of y: ')
+
+#create a temporary var and swap values
+temp = x
+x = y
+y = temp
+
+print('The values of x after swapping: {}'.format(x))
+print('The values of y after swapping: {}'.format(y))
