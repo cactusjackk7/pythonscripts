31806bdd6024cf21bd93e64bafc1600f3c7cf560
cactusjackk7 <cactusjackk7@gmail.com>
2021-01-12 22:34:59 +0100

    convert kilometers to miles

---
+++ km.py
@@ -0,0 +1,9 @@
+# taking kms input from user
+kilometers = float(input("Enter value in kms: "))
+
+#conversion factor
+conv_fac = 0.621371
+
+#calculate files
+miles = kilometers * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
