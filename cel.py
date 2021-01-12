bc472498bed2088b68c6cb2ae126576ca5b245e0
cactusjackk7 <cactusjackk7@gmail.com>
2021-01-12 22:56:33 +0100

    convert Celsius to Fahrenheit

---
+++ cel.py
@@ -0,0 +1,9 @@
+#python program to convert tempature to fahrenheit
+
+#change this value for a different result
+celsius: 32.5
+
+#calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print("%0.1f degree Celsius is equal to %0.1f degree fahrenheit" %(celsius, fahrenheit))
+
